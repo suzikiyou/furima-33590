969878b287d5e549528ed0223fefa31c93cd3145
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-02 09:43:48 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX, message: "は半角6文字以上英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
 validates :nickname, presence: true
-
+validates :birth date , presence: true
+validates :last name, presence: true
+validates :last name,format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+validates :first name, presence: true
+validates :first name,format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+validates :last name kana, presence: true
+validates :last name kana,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana  characters." }
+validates :first name kana, presence: true
+validates :first name kana, { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana  characters." }
 end
