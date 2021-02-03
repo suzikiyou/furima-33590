2ddfeb3473a985ad067cecfe8fb9aa8a8eba41f0
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-03 15:31:37 +0900

    モデルの単体テストコード#2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname                     { 'furima' }
+    email                        { 'test@example' }
+    password                     { 'you1115' }
+    password_confirmation        { password }
+    last_name                    { '山田' }
+    first_name                   { '太郎' }
+    last_name_kana               { 'ヤマダ' }
+    first_name_kana              { 'タロウ' }
+    birth_date                   { '2000-01-01'}
   end
 end
