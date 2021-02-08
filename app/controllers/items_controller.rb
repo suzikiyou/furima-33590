c33e99c8f0d5efbf38975654003a4b4621ee6ff1
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-08 18:53:32 +0900

    手順1-3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.all
   end
 
   def new
@@ -16,9 +18,16 @@ class ItemsController < ApplicationController
      end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :proprietary_name, :product_description, :category_id , :condition_id, :shipping_charge_id, :prefectures_id, :days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
