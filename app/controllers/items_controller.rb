308183eb15fc952aaeffc843f2c3c6776f8d637b
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-08 19:33:02 +0900

    修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.all
   end
