efced830876fc78a481956fd47effadeea151b5c
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-09 19:13:12 +0900

    修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order("created_at ASC")
   end
@@ -33,12 +33,13 @@ class ItemsController < ApplicationController
     end
   end
 
- def destroy
-   if current_user.id == @item.user.id
-     @item.destroy
-     redirect_to root_path
-   end
- end
+  def destroy
+    if @item.destroy
+       redirect_to root_path
+    else
+       render :show
+    end
+  end
 
 
   private
