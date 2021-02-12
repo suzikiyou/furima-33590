f9359ca91d0a1b84e8d091608f64f7d926bbcd21
suzikiyou <76513352+suzikiyou@users.noreply.github.com>
2021-02-12 19:33:12 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
-    @items = Item.includes(:user).order("created_at ASC")
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -53,7 +53,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user
+    if current_user.id != @item.user.id
      redirect_to action: :index
     end
   end
